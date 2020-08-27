e8b52f6aa00649bfe2c37559987241d43a04583a
Raptor123471 <raptor123471@gmail.com>
2020-08-27 10:57:24 -0400

    Revert "Remove double search"
    This reverts commit 7ef9eceda3879e78caa5f5e2be09f9d31370e571.

--- musicbot/audiocontroller.py
+++ musicbot/audiocontroller.py
@@ -139,6 +139,21 @@ class AudioController(object):
                 await self.play_soundcloud(track)
             return
 
+        # If the track is a video title, get the corresponding video link first
+        if not ("watch?v=" in track):
+
+            # shortlink support
+            if "youtu.be" in track:
+                link = linkutils.convert_shortlink(track)
+
+            else:
+                link = await self.search_youtube('"' + track + '"')
+                if link is None:
+                    link = await self.search_youtube(track)
+                    if link is None:
+                        return
+        else:
+            link = track
         self.playlist.add(link)
         if len(self.playlist.playque) == 1:
             print("Playing {}".format(link))

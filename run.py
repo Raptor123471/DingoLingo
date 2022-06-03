33c72b152d8a5ab625a83cda111d7a878f6566c2
unknown <hapzazad@gmail.com>
2022-06-03 08:22:30 +0300

    rewritten run.py to object-oriented style

--- run.py
+++ run.py
@@ -10,47 +10,49 @@ from musicbot.utils import guild_to_audiocontroller, guild_to_settings
 
 initial_extensions = ['musicbot.commands.music',
                       'musicbot.commands.general', 'musicbot.plugins.button']
-bot = commands.Bot(command_prefix=config.BOT_PREFIX,
+
+
+class MusicBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        print(config.STARTUP_MESSAGE)
+
+        for guild in self.guilds:
+            await self.register(guild)
+            print("Joined {}".format(guild.name))
+
+        print(config.STARTUP_COMPLETE_MESSAGE)
+
+    async def on_guild_join(self, guild):
+        print(guild.name)
+        await self.register(guild)
+
+    async def register(self, guild: discord.Guild):
+        if guild in guild_to_settings:
+            return
+
+        sett = guild_to_settings[guild] = Settings(guild)
+        controller = guild_to_audiocontroller[guild] = AudioController(bot, guild)
+
+        if config.GLOBAL_DISABLE_AUTOJOIN_VC:
+            return
+
+        if not sett.get('vc_timeout'):
+            try:
+                await controller.register_voice_channel(
+                    guild.get_channel(sett.get('start_voice_channel'))
+                    or guild.voice_channels[0]
+                )
+            except Exception as e:
+                print(e)
+
+
+bot = MusicBot(command_prefix=config.BOT_PREFIX,
                    pm_help=True, case_insensitive=True,
                    status=discord.Status.online,
                    activity=discord.Game(name="Music, type {}help".format(config.BOT_PREFIX)))
-
-
-@bot.event
-async def on_ready():
-    print(config.STARTUP_MESSAGE)
-
-    for guild in bot.guilds:
-        await register(guild)
-        print("Joined {}".format(guild.name))
-
-    print(config.STARTUP_COMPLETE_MESSAGE)
-
-
-@bot.event
-async def on_guild_join(guild):
-    print(guild.name)
-    await register(guild)
-
-
-async def register(guild: discord.Guild):
-    if guild in guild_to_settings:
-        return
-
-    sett = guild_to_settings[guild] = Settings(guild)
-    controller = guild_to_audiocontroller[guild] = AudioController(bot, guild)
-
-    if config.GLOBAL_DISABLE_AUTOJOIN_VC:
-        return
-
-    if not sett.get('vc_timeout'):
-        try:
-            await controller.register_voice_channel(
-                guild.get_channel(sett.get('start_voice_channel'))
-                or guild.voice_channels[0]
-            )
-        except Exception as e:
-            print(e)
 
 
 if __name__ == '__main__':

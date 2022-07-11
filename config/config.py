560faffaf71f98dcfb474751b2c842f3e93a6b74
unknown <hapzazad@gmail.com>
2022-07-11 11:36:05 +0300

    configure for heroku

--- config/config.py
+++ config/config.py
@@ -17,7 +17,7 @@ MAX_SONG_PRELOAD = 5  # maximum of 25
 
 # database url in SQL Alchemy-supported format, must be async-compatible
 # CHANGE ONLY IF YOU KNOW WHAT YOU'RE DOING
-DATABASE = "sqlite+aiosqlite:///settings.db"
+DATABASE = os.getenv("DATABASE_URL").replace("postgres", "postgresql+asyncpg", 1)
 
 COOKIE_PATH = "/config/cookies/cookies.txt"
 

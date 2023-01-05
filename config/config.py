2ee7807fef5910eac01416e3ca171248d6b67de1
Silas Palkowski <pasi2708@gmx.de>
2023-01-05 12:17:18 +0100

    fixing some issues again

--- config/config.py
+++ config/config.py
@@ -42,6 +42,19 @@ actual_prefix = (  # for internal use
     else ("/" if ENABLE_SLASH_COMMANDS else "@bot ")
 )
 
+# database url in SQL Alchemy-supported format, must be async-compatible
+# CHANGE ONLY IF YOU KNOW WHAT YOU'RE DOING
+DATABASE = "sqlite+aiosqlite:///settings.db"
+if os.getenv("HEROKU"):
+    # example with Heroku Postgres
+    DATABASE = os.getenv(
+        "DATABASE_URL",  # environment variable with the DB url
+        "postgres",  # default url (as env vars are not available at build time)
+    ).replace(
+        "postgres", "postgresql+asyncpg", 1  # make url supported by SQL Alchemy
+    )
+    # another example with MySQL
+    # DATABASE = os.getenv("DATABASE_URL", "mysql").replace("mysql", "mysql+aiomysql", 1)
 
 
 STARTUP_MESSAGE = "Starting Bot..."
--- config/setup.py
+++ config/setup.py
@@ -1,26 +1,10 @@
 "This file is here to automatically install the selected DB package"
-import os
 from setuptools import setup
+from config import DATABASE
 
-# database url in SQL Alchemy-supported format, must be async-compatible
-# CHANGE ONLY IF YOU KNOW WHAT YOU'RE DOING
-DATABASE = "sqlite+aiosqlite:///settings.db"
-if os.getenv("HEROKU"):
-    # example with Heroku Postgres
-    DATABASE = os.getenv(
-        "DATABASE_URL",  # environment variable with the DB url
-        "postgres",  # default url (as env vars are not available at build time)
-    ).replace(
-        "postgres", "postgresql+asyncpg", 1  # make url supported by SQL Alchemy
-    )
-    # another example with MySQL
-    # DATABASE = os.getenv("DATABASE_URL", "mysql").replace("mysql", "mysql+aiomysql", 1)
 
 setup(
     name="MusicBot DB",
     install_requires=[DATABASE.partition("+")[2].partition(":")[0]],
 )
-
-
-
 

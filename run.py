47479f7849a41f0a317ab6c0475da7c8e7501aef
Krutyi-4el <60041069+Krutyi-4el@users.noreply.github.com>
2023-02-25 17:19:18 +0200

    improve signal and error handling

--- run.py
+++ run.py
@@ -9,7 +9,8 @@ if "--run" in sys.argv:
     import runpy
 
     runpy.run_module("musicbot", run_name="__main__")
-    exit()
+    # reminder: there's no `exit` in frozen environment
+    sys.exit()
 
 import os
 import signal
@@ -34,8 +35,6 @@ if on_windows:
         if event == signal.CTRL_C_EVENT:
             os.kill(child_pid, signal.SIGTERM)
 
-    assert SetHandler(handler, True), "failed to set Ctrl+C handler"
-
     kwargs = {
         "creationflags": subprocess.CREATE_NO_WINDOW
         | subprocess.CREATE_NEW_PROCESS_GROUP
@@ -52,19 +51,42 @@ p = subprocess.Popen(
     **kwargs,
 )
 
+
+# disable interrupting until we have the pid
+# (we don't want to have a wild process at this point)
+default_sigint_handler = signal.signal(signal.SIGINT, lambda s, f: None)
+
+# the pid is passed via stdout because p.pid can be incorrect
 line = p.stdout.readline()
 try:
     child_pid = int(line)
 except ValueError:
     print("Can't grab subprocess id, something is wrong!")
     print("The output is:", line, sep="\n", end="")
+else:
+    if on_windows:
+        assert SetHandler(handler, True), "failed to set Ctrl+C handler"
+
+
+def new_handler(sig, frame):
+    """Handle the first interrupt and ignore others
+    to prevent showing error instead of subprocess output"""
+    global default_sigint_handler
+    h = default_sigint_handler
+    if h:
+        default_sigint_handler = None
+        h(sig, frame)
+
+
+signal.signal(signal.SIGINT, new_handler)
 
 try:
     while line := p.stdout.readline():
         print(line, end="")
 except KeyboardInterrupt:
     if not on_windows:
-        p.send_signal(signal.SIGINT)
+        os.kill(child_pid, signal.SIGINT)
+    print(p.stdout.read(), end="")
 
 if p.wait() != 0:
-    input("Press Enter to exit...")
+    input("Press Enter to exit... ")
